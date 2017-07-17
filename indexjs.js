ea665cc5cccf67126203f2a50844fd796c591dff
Demo User <demouser@MacBook-Pro.local>
2017-07-17 12:51:18 -0700

    Monday afternoon update

--- indexjs.js
+++ indexjs.js
@@ -1,27 +1,25 @@
-/*function addComment() {
+/*
+function addComment() {
   var text = $('#commentBox').val();
-  list = document.querySelector('ul');
+  list = document.querySelector('#previousComments');
   item = document.createElement('li');
   item.innerText = text;
   list.appendChild(item);
 }
 */
+var numComment = 0;
 
 function addComment(){
-  var text = $("commentBox").val();
-  $("commentSection").append(text);
+  var text = $("#commentBox").val();
+  if (numComment > 0) {
+    $("#previousComments").prepend("<hr>");
+  }
+  $("#previousComments").prepend("<p>"+text+"</p>");
+  numComment = numComment + 1;
 }
 function setup() {
-  $("#ok_button").click(addListItem);
-
+  $("#ok_button").click(addComment);
 }
 
 $(document).ready(setup);
 addComment("from JS file");
-
-function addListItem(text){
-  list = document.$('#previousComments ul');
-  item = document.createElement('li');
-  item.innerText = text;
-  list.appendChild(item);
-}
